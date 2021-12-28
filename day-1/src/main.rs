053e0d787cb49c9dc93565e5fb8642ecf1537911
Max Nicholson <max.nicholson@calmid.com>
2021-12-28 17:50:27 +0000

    feat: day-1(b)

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,18 +1,36 @@
 fn main() {
-    let depths: Vec<usize> = include_str!("../input.txt")
+    let depths = read_depths();
+    println!("a: {}", a(depths.clone()));
+    println!("b: {}", b(depths));
+}
+
+fn read_depths() -> Vec<usize> {
+    include_str!("../input.txt")
         .lines()
         .map(|line| line.parse::<usize>().unwrap())
+        .collect()
+}
+
+fn a(depths: Vec<usize>) -> usize {
+    depths.iter().zip(depths.iter().skip(1)).fold(
+        0,
+        |acc, (prev, curr)| {
+            if curr > prev {
+                acc + 1
+            } else {
+                acc
+            }
+        },
+    )
+}
+
+fn b(depths: Vec<usize>) -> usize {
+    let windows: Vec<usize> = depths
+        .windows(3)
+        .map(|window| window.iter().sum())
         .collect();
-    let increments =
-        depths.iter().zip(depths.iter().skip(1)).fold(
-            0,
-            |acc, (prev, curr)| {
-                if curr > prev {
-                    acc + 1
-                } else {
-                    acc
-                }
-            },
-        );
-    println!("{}", increments);
+    windows
+        .iter()
+        .zip(windows.iter().skip(1))
+        .fold(0, |acc, (prev, curr)| acc + (curr > prev) as usize)
 }
