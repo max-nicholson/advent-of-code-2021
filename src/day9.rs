1ad78c648d3e85b484e13e5bc40b35b8d9983e72
Max Nicholson <max.nicholson@quickrelease.co.uk>
2022-01-03 14:32:32 +0000

    feat: day-9 part_2

--- src/day9.rs
+++ src/day9.rs
@@ -1,3 +1,8 @@
+use std::{
+    cmp::Reverse,
+    collections::{BinaryHeap, HashSet, VecDeque},
+};
+
 use aoc_runner_derive::aoc;
 
 #[aoc_generator(day9)]
@@ -10,35 +15,82 @@ fn parse_input(input: &str) -> Vec<Vec<u32>> {
 
 #[aoc(day9, part1)]
 pub fn part_1(grid: &[Vec<u32>]) -> u32 {
-    let row = &grid[0];
-    let mut troughs = Vec::new();
-    let neighbours: Vec<(i32, i32)> = vec![(0, 1), (1, 0), (-1, 0), (0, -1)];
+    let troughs = find_troughs(grid);
+    troughs.iter().map(|&(x, y)| grid[y][x] + 1).sum()
+}
 
-    let max_x: i32 = row.len().try_into().unwrap();
+#[aoc(day9, part2)]
+pub fn part_2(grid: &[Vec<u32>]) -> u32 {
+    let neighbours: Vec<(i32, i32)> = vec![(0, 1), (1, 0), (-1, 0), (0, -1)];
+    let troughs = find_troughs(grid);
+    let (max_x, max_y) = get_bounds(grid);
+    let basins = troughs.iter().map(|&(basin_x, basin_y)| {
+        // Each basin has a minimum size of 3-5 (itself, plus all neighbours in bounds)
+        let mut size: u32 = 0;
+        let mut stack: VecDeque<(usize, usize)> = VecDeque::from([(basin_x, basin_y)]);
+        let mut seen: HashSet<(usize, usize)> = HashSet::new();
+        while !stack.is_empty() {
+            let (x, y) = stack.pop_front().unwrap();
+
+            if !seen.insert((x, y)) || grid[y][x] == 9{
+                continue;
+            };
+            size += 1;
+
+            stack.extend(
+                neighbours
+                    .iter()
+                    .map(|(delta_x, delta_y)| (x as i32 + delta_x, y as i32 + delta_y)) // Bounds check
+                    .filter(|&(x, y)| x >= 0 && y >= 0 && x < max_x && y < max_y)
+                    .map(|(x, y)| (x as usize, y as usize)),
+            );
+        }
+        size
+    });
+    select_k(basins, 3).into_iter().product()
+}
+
+fn select_k(iter: impl Iterator<Item = u32>, k: usize) -> Vec<u32> {
+    let mut h = BinaryHeap::new();
+    for item in iter {
+        h.push(Reverse(item));
+        if h.len() > k {
+            h.pop();
+        }
+    }
+    h.into_iter().map(|rev| rev.0).collect()
+}
+
+fn get_bounds(grid: &[Vec<u32>]) -> (i32, i32) {
+    let max_x: i32 = grid[0].len().try_into().unwrap();
     let max_y: i32 = grid.len().try_into().unwrap();
+
+    (max_x, max_y)
+}
+
+fn find_troughs(grid: &[Vec<u32>]) -> Vec<(usize, usize)> {
+    let neighbours: Vec<(i32, i32)> = vec![(0, 1), (1, 0), (-1, 0), (0, -1)];
+    let mut troughs: Vec<(usize, usize)> = Vec::new();
+
+    let (max_x, max_y) = get_bounds(grid);
 
     for (y, row) in grid.iter().enumerate() {
         for (x, &cell) in row.iter().enumerate() {
             if neighbours
-                    .iter()
-                    // Find neighbours
-                    .map(|(delta_x, delta_y)| (x as i32 + delta_x, y as i32 + delta_y))
-                    // Bounds check
-                    .filter(|&(x, y)| x >= 0 && y >= 0 && x < max_x && y < max_y)
-                    // Find smaller neighbour
-                    .find(|&(x, y)| grid[y as usize][x as usize] <= cell)
-                    == None
+                .iter()
+                // Find neighbours
+                .map(|(delta_x, delta_y)| (x as i32 + delta_x, y as i32 + delta_y))
+                // Bounds check
+                .filter(|&(x, y)| x >= 0 && y >= 0 && x < max_x && y < max_y)
+                // Find smaller neighbour
+                .find(|&(x, y)| grid[y as usize][x as usize] <= cell)
+                == None
             {
-                troughs.push(cell);
+                troughs.push((x, y));
             }
         }
     }
-    troughs.iter().map(|height| height + 1).sum()
-}
-
-#[aoc(day9, part2)]
-pub fn part_2(input: &[Vec<u32>]) -> u32 {
-    todo!();
+    troughs
 }
 
 #[cfg(test)]
@@ -58,6 +110,6 @@ mod tests {
 
     #[test]
     fn test_part_2() {
-        //assert_eq!(part_2(&parse_input(EXAMPLE)), 61229);
+        assert_eq!(part_2(&parse_input(EXAMPLE)), 1134);
     }
 }
